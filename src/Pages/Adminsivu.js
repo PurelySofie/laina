60ca4f3e5b2414c988ae15ce45af902177699cc9
Akonpelto Anton <gr275806@gradia.fi>
2023-09-05 16:19:21 +0300

    Lisätty Dev-branchin muutoksia

--- src/Pages/Adminsivu.js
+++ src/Pages/Adminsivu.js
@@ -1,12 +1,23 @@
 import React from "react"
 import "./Adminsivu.css"
+import ListItems from "../components/Admin-Sivu/ListItems"
+
+// JSON-Import
+// Väliaikanen
+import kirjaData from "../Databases/Lainat.json"
 
 function Adminsivu(){
     return(
         <div>
             <h1>Täällä muutetaan lainauksia</h1>
+            <br />
+            {
+                kirjaData.map(kirja =>
+                    <ListItems kirja={kirja} />
+                )
+            }
         </div>
-    );
+    )
 }
 
 export default Adminsivu;
