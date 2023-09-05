dc61823dd494d9e8b548ab6bb0c29d4a58967009
PurelySofie <sofifineko@gmail.com>
2023-09-05 13:25:20 +0300

    Paljon muutoksia
    Tein lisää sivuja valmiiksi sekä routtasin ne jotta /lainasivu menee lainasivulle.
    Tein myös simppelin navbarin ja lisäsin kaikille uusille tiedostoille .css tiedoston.

--- src/Pages/Adminsivu.js
+++ src/Pages/Adminsivu.js
@@ -1,1 +1,12 @@
-//hyödytön teksti
+import React from "react"
+import "./Adminsivu.css"
+
+function Adminsivu(){
+    return(
+        <div>
+            <h1>Täällä muutetaan lainauksia</h1>
+        </div>
+    );
+}
+
+export default Adminsivu;
