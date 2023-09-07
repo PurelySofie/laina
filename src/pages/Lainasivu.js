c6c9af4550bd923fbeddb74fc2a5c6d8482417ab
PurelySofie <sofifineko@gmail.com>
2023-09-07 09:15:36 +0300

    Lainasivu.js

--- src/pages/Lainasivu.js
+++ src/pages/Lainasivu.js
@@ -1,12 +1,43 @@
-import React from "react"
-import "./Lainasivu.css"
+//import React from "react"
+///import "./Lainasivu.css"
 
-function Lainasivu(){
-    return(
-        <div className="content">
-            <h1>Täällä lainataan</h1>
-        </div>
-    );
+//function Lainasivu(){
+   // return(
+        //<div>
+           // <h1>Täällä lainataan</h1>
+        //</div>
+   // );
+//}
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function KirjojenLainausSivu() {
+  const { kirjaId } = useParams();
+  const [kirja, setKirja] = useState(null);
+
+  useEffect(() => {
+    // Tässä voit tehdä HTTP-pyynnön hakeaksesi kirjan tiedot kirjaId:n perusteella
+    fetch(`/api/kirjat/${kirjaId}`)
+      .then((response) => response.json())
+      .then((data) => setKirja(data))
+      .catch((error) => console.error("Virhe kirjatietojen haussa: ", error));
+  }, [kirjaId]);
+
+  if (!kirja) {
+    return <div>Ladataan kirjatietoja...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Kirjan tiedot</h1>
+      <h2>{kirja.nimi}</h2>
+      <p>Kirjailija: {kirja.kirjailija}</p>
+      <p>Julkaisuvuosi: {kirja.julkaisuvuosi}</p>
+      <p>Saatavuus: {kirja.saatavuus ? "Saatavilla" : "Ei saatavilla"}</p>
+      <button>Lainaa kirja</button>
+    </div>
+  );
 }
 
-export default Lainasivu;
+export default KirjojenLainausSivu;
