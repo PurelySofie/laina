8825a8cd45d29d3fbb925a9fd491877849118d1e
jeremyllyneva <jeremyllyneva02@gmail.com>
2023-09-07 10:48:44 +0300

    kirjaudu

--- src/pages/Uusitili.js
+++ src/pages/Uusitili.js
@@ -15,32 +15,25 @@ const Uusitili = () => {
                 <h1 className="title-rekisteröidy">Rekisteröidy</h1>
 
                 <div className="inputContainer">
-                    <input type="text" className="input" placeholder="a" required />
-                    <label htmlFor="" className="label">Etunimi</label>
-                </div>
-
-                <div className="inputContainer">
-                    <input type="text" className="input" placeholder="a" required />
-                    <label htmlFor="" className="label">Sukunimi</label>
-                </div>
-
-                <div className="inputContainer">
                     <input type="email" className="input" placeholder="a" required />
                     <label htmlFor="" className="label">Sähköposti</label>
                 </div>
 
                 <div className="inputContainer">
-                <input type={passwordShown ? "text" : "password"} className="input" name="password" placeholder="a" required />
+                    <input type={passwordShown ? "text" : "password"} className="input" name="password" placeholder="a" required />
                     <label htmlFor="" className="label">Salasana</label><br></br>
                 </div>
-                <input type="checkbox" className="showPsw" onClick={togglePassword}/> Näytä Salasana
+                <div className="showPassword">
+                    <input type="checkbox" className="showPsw" onClick={togglePassword} />
+                    <p className='näytä'>Näytä Salasana</p>
+                </div>
 
                 <input type="submit" className="submitBtn" value="Rekisteröidy" />
 
                 <div className="kirjaudu">
                   <h4 className="hr-lines">TAI</h4>
                   <p>Onko sinulla jo käyttäjä?</p>
-                  <Link to="/Loginsivu">Kirjaudu Sisään</Link>
+                  <Link to="/login">Kirjaudu Sisään</Link>
                 </div>
             </form>
         </div>
@@ -48,3 +41,16 @@ const Uusitili = () => {
 }
 
 export default Uusitili;
+
+
+
+
+/*<div className="inputContainer">
+    <input type="text" className="input" placeholder="a" required />
+    <label htmlFor="" className="label">Etunimi</label>
+</div>
+
+<div className="inputContainer">
+    <input type="text" className="input" placeholder="a" required />
+    <label htmlFor="" className="label">Sukunimi</label>
+</div>*/
