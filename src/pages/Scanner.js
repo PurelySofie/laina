9db012f7c09c0187f6be068b808f966d096a81b3
HuismanHugo <hugo06.huisman@gmail.com>
2023-09-20 10:39:02 +0300

    Muutoksia

--- src/pages/Scanner.js
+++ src/pages/Scanner.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 import './Scanner.css';
 
+
 function Scanner() {
   const [scanResult, setScanResult] = useState(null);
-  const [manualSerialNumber, setManualSerialNumber] = useState('');
 
   useEffect(() => {
     const scanner = new Html5QrcodeScanner('reader', {
       qrbox: {
-        width: 300, // Leventä QR-skanneria
-        height: 300, // Korota QR-skanneria
+        width: 300,
+        height: 300,
       },
       fps: 5,
     });
@@ -37,32 +37,17 @@ function Scanner() {
     };
   }, []);
 
-  function handleManualSerialNumberChange(event) {
-    setManualSerialNumber(event.target.value);
-  }
 
   return (
     <div className="App">
-      <h1>QR Scanning Code</h1>
       {scanResult ? (
         <div>
           <p>Success: <a href={scanResult}>{scanResult}</a></p>
-          <p>Serial Number: {scanResult.slice(-16)}</p>
         </div>
       ) : (
         <div>
           <div id="reader" className="center-scanner"></div>
-          <p className="center-text">Or enter the serial number manually:</p>
-          <div className="center-input">
-            <input
-              type="text"
-              value={manualSerialNumber}
-              onChange={handleManualSerialNumberChange}
-            />
-            {manualSerialNumber && (
-              <p>Serial Number: {manualSerialNumber.slice(-16)}</p>
-            )}
-          </div>
+          
         </div>
       )}
     </div>
