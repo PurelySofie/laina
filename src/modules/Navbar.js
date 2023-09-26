40180dba0827c28db012de5a4ea68909be72f309
Pietu1234 <124781494+Pietu1234@users.noreply.github.com>
2023-09-26 13:13:39 +0300

    navbar melkein kunnossa
    mulla ja tuomaksella alkaa kt niin sinne pitää lähteä ainoa vika tässä on että kun näyttöä pienentää se navbar nappi ei jää vaan katoaa

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -1,34 +1,43 @@
-import React from "react"
-import { Outlet, Link} from "react-router-dom"
-import "./Navbar.css"
+import React, { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <>
-        <div className="banner">
-            <div className="navbar">
-                <ul>
-                    <li>
-                        <Link to="/">Etusivu</Link>
-                    </li>
-                    <li>
-                        <Link to="/lainaa">Lainasivu</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin">Adminsivu</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Kirjaudu sisään</Link>
-                    </li>
-                    <li>
-                        <Link to="/userpage">Omat lainat</Link>
-                    </li>
-                </ul>
-            </div>
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <>
+      <div className={`navbar ${isMobileMenuOpen ? "mobile-menu-open" : ""}`}>
+        <div className="menu-toggle" onClick={toggleMobileMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
         </div>
-<Outlet />
-</>
-    );
+        <ul className={`nav-list ${isMobileMenuOpen ? "show" : ""}`}>
+          <li>
+            <Link to="/">Etusivu</Link>
+          </li>
+          <li>
+            <Link to="/lainaa">Lainasivu</Link>
+          </li>
+          <li>
+            <Link to="/admin">Adminsivu</Link>
+          </li>
+          <li>
+            <Link to="/login">Kirjaudu sisään</Link>
+          </li>
+          <li>
+            <Link to="/userpage">Omat lainat</Link>
+          </li>
+        </ul>
+      </div>
+      <Outlet />
+    </>
+  );
 }
 
 export default Navbar;
